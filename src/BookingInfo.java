1cc4e19d39b1c62c092d9dee79a5272d859c817e
Nokkvi <nos1@hi.is>
2016-03-23 14:23:30 +0000

    Constructors

--- src/BookingInfo.java
+++ src/BookingInfo.java
@@ -1,6 +1,15 @@
 
 public class BookingInfo {
-
+	private User user;
+	private IndivDayTrip trip;
+	private int seats;
+	
+	public BookingInfo(User u, IndivDayTrip t, int s){
+		this.user = u;
+		this.trip = t;
+		this.seats = s;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Þetta er BookingInfo Klasinn");
 
