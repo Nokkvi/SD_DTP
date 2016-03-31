483499911b4407fffce90d5e4b83b88e331a2c63
Nokkvi <nos1@hi.is>
2016-03-31 20:07:05 +0000

    database upphaf

---
+++ database.sql
@@ -0,0 +1,13 @@
+﻿-- Table: "Daytrips"."Rating"
+
+-- DROP TABLE "Daytrips"."Rating";
+
+CREATE TABLE "Daytrips"."Rating"
+(
+
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Daytrips"."Rating"
+  OWNER TO postgres;
