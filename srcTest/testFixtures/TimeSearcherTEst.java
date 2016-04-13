8f8e02f7f9e8f94c77a0143ddcb31eb478e35076
Nokkvi <nos1@hi.is>
2016-04-13 15:18:16 +0000

    meh

--- srcTest/testFixtures/TimeSearcherTEst.java
+++ srcTest/testFixtures/TimeSearcherTEst.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
-import mockObjects.*;
+import mockObjects.M_IndivDayTripList_Time;
 import interfaces.*;
 
 import org.junit.After;
@@ -13,72 +13,33 @@ import org.junit.Test;
 import classes.*;
 
 public class TimeSearcherTEst {
-	private Searcher searchG;
-	private Searcher searchL;
-	private Searcher searchE;
-	
+	private Searcher search;
+
 
 	@Before
 	public void setUp() throws Exception {
-		I_IndivDayTripList tGen = new M_IndivDayTripList_Time();
-		I_IndivDayTripList tLar = new M_IndivDayTripList_TimeL();
-		I_IndivDayTripList tEmp = new M_IndivDayTripList_TimeE();
-		
-		searchG = new Searcher(tGen);
-		searchL = new Searcher(tLar);
-		searchE = new Searcher(tEmp);
-	}
-	
-	@Test
-	public void testGeneral() {
-		System.out.println("Testing with general Input");
-		Info[] k = searchG.searchByTime(new Date(116,8,12,18,11),new Date(117,8,12,18,11));
-		for(int i = 0; i < k.length;i++){
-			String blee = "";
-			int id = 0;
-			id = k[i].getIndivId();
-			blee = k[i].getName();
-			System.out.println(id+" "+blee);
-		}
-		
-	}
-	
-	@Test
-	public void testLarge() {
-		System.out.println("Testing with large Input");
-		Info[] k = searchL.searchByTime(new Date(116,6,12,18,11),new Date(117,8,12,18,11));
-		for(int i = 0; i < k.length;i++){
-			String blee = "";
-			int id = 0;
-			id = k[i].getIndivId();
-			blee = k[i].getName();
-			System.out.println(id+" "+blee);
-		}
-		
-	}
-	
-	@Test
-	public void testEmpty() {
-		System.out.println("Testing with input that returns no usable values");
-		Info[] k = searchE.searchByTime(new Date(117,8,12,18,11),new Date(117,8,12,18,11));
-		for(int i = 0; i < k.length;i++){
-			String blee = "";
-			int id = 0;
-			id = k[i].getIndivId();
-			blee = k[i].getName();
-			System.out.println(id+" "+blee);
-		}
-		
+		I_IndivDayTripList i = new M_IndivDayTripList_Time();
+		search = new Searcher(i);
 	}
 
+	@Test
+	public void test() {
+		Info[] k = search.searchByTime(new Date(116,8,12,18,11),new Date(117,8,12,18,11));
+		for(int i = 0; i < k.length;i++){
+			String blee = "";
+			int id = 0;
+			id = k[i].getIndivId();
+			blee = k[i].getName();
+			System.out.println(id+" "+blee);
+		}
+
+	}
 
 	@After
 	public void tearDown() throws Exception {
-		searchG = null;
-		searchE = null;
-		searchL = null;
+		search = null;
 	}
 
-	
+
 
 }
