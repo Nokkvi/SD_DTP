59afffc66afac78e5a1bdeb23560b3b27f2d0282
Nokkvi <nos1@hi.is>
2016-04-14 17:16:31 +0000

    database update 3

--- src/Classes/Database.java
+++ src/Classes/Database.java
@@ -6,11 +6,14 @@ import interfaces.I_Database;
 import java.sql.*;
 
 public class Database implements I_Database {
+	static Connection c;
+	static Statement stmt;
+
 
 	  public static void main( String args[] )
 	  {
-		    Connection c = null;
-		    Statement stmt = null;
+		    c = null;
+		    stmt = null;
 		    try {
 		      Class.forName("org.sqlite.JDBC");
 		      c = DriverManager.getConnection("jdbc:sqlite:Database.db");
@@ -18,38 +21,26 @@ public class Database implements I_Database {
 		      System.out.println("Opened database successfully");
 
 		      stmt = c.createStatement();
-		      
+		      /*
 		      String sql = "INSERT INTO COMPANY(NAME,SIMANUMER,EMAIL)" +
 		    		  	   "VALUES('Reykjavik Excursions', '5805400','main@re.is')";
 		      stmt.executeUpdate(sql);
+		      */
 		      
-		      /*
 		      String sql = "INSERT INTO Daytrips(NAME,COMPANY,PRICE,DESCR,KEYWORDS,CATEGORY,HOTELPICKUP) " +
-		                   "VALUES('Essential Iceland', 'Reykjavik Excursions',42900, 'Enjoy an amazing tour through a landscape of extreme contrasts, full of history and geology.',"+
-		                   "'Thingvellir - Vikings - Kaldidalur - Langjokull - Lava field - Waterfalls - Hot Springs', 'Adventure', 'No Pickup');";
-		      stmt.executeUpdate(sql);
-		      */
-		      /*
-		      String sql = "INSERT INTO indivDaytrips(ID,STARTTIME,ENDTIME,DAYTRIP,NUMSEATSAVAIL) " +
-		    		  	   "VALUES(0096, '2016-04-22 09:00:00.000', '2016-04-22 19:00:00.000', 'Essential Iceland', 20);";
+		                   "VALUES('Snorkeling in Silfra', 'Reykjavik Excursions',46900, 'Snorkeling in Iceland is an activity that everyone who has learnt to swim can enjoy.',"+
+		                   "'Thingvellir - Geysir - Gullfoss - Super jeep - Snowmobiling', 'Adventure', 'No Pickup');";
 		      stmt.executeUpdate(sql);
 		      
-		      sql = "INSERT INTO indivDaytrips(ID,STARTTIME,ENDTIME,DAYTRIP,NUMSEATSAVAIL) " +
-	    		  	   "VALUES(0097, '2016-04-23 09:00:00.000', '2016-04-23 19:00:00.000', 'Essential Iceland', 20);";
-		      stmt.executeUpdate(sql);
 		      
 		      sql = "INSERT INTO indivDaytrips(ID,STARTTIME,ENDTIME,DAYTRIP,NUMSEATSAVAIL) " +
-	    		  	   "VALUES(009, '2016-04-24 09:00:00.000', '2016-04-24 19:00:00.000', 'Essential Iceland', 20);";
+		    		  	   "VALUES(0102, '2016-04-20 09:00:00.000', '2016-04-20 19:00:00.000', 'Super Jeep & Snowmobiling', 20);";
 		      stmt.executeUpdate(sql);
 		      
-		      sql = "INSERT INTO indivDaytrips(ID,STARTTIME,ENDTIME,DAYTRIP,NUMSEATSAVAIL) " +
-	    		  	   "VALUES(0094, '2016-04-25 09:00:00.000', '2016-04-25 19:00:00.000', 'Essential Iceland', 20);";
-		      stmt.executeUpdate(sql);
 		      
-		      sql = "INSERT INTO indivDaytrips(ID,STARTTIME,ENDTIME,DAYTRIP,NUMSEATSAVAIL) " +
-	    		  	   "VALUES(0099, '2016-04-28 09:00:00.000', '2016-04-28 19:00:00.000', 'Essential Iceland', 20);";
-		      stmt.executeUpdate(sql);
-		      */
+		      
+		      
+		      
 		      stmt.close();
 		      c.commit();
 		      c.close();
@@ -59,4 +50,27 @@ public class Database implements I_Database {
 		    }
 		    System.out.println("Table created successfully");
 		  }
+	  
+	  public static void connectToDatabase(){
+		    c = null;
+		    stmt = null;
+		    try {
+		      Class.forName("org.sqlite.JDBC");
+		      c = DriverManager.getConnection("jdbc:sqlite:Database.db");
+		      c.setAutoCommit(false);
+		      System.out.println("Opened database successfully");		
+		    } catch ( Exception e ) {      
+		    }
+	  }
+	  public static void closeDatabase(){
+		  try {
+		  stmt.close();
+	      c.commit();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.out.println("Database closed");
+	    }
+	    System.out.println("Table created successfully"); 
 	}
+}
