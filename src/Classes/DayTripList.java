34a373155b40c6f27e3cb36345a33109d01738c1
MrZoinker <mrzoinker@gmail.com>
2016-04-14 19:00:31 +0000

    addDayTrip útfærsla

--- src/Classes/DayTripList.java
+++ src/Classes/DayTripList.java
@@ -6,11 +6,46 @@ import java.sql.*;
 
 
 public class DayTripList implements I_DayTripList {
-
+	
     @Override
     public void addDayTrip(DayTrip d) {
-        // TODO Auto-generated method stub
-        
+		String name = d.getName();
+		String cat = d.getCategory();
+		String[] pickup = d.getPickups();
+		String desc = d.getDesc();
+		String loc = d.getRegion();
+		String[] keyw = d.getKeywords();
+		String price = Integer.toString(d.getPrice());
+		String[] comp = d.getCompany();
+		
+		String sPickup = "";
+		String sKeyw = "";
+		
+		if(pickup.length > 0){
+			sPickup = pickup[0];
+			for(int i = 1; i < pickup.length; i++){
+				sPickup = sPickup + ", " + pickup[i];
+			}
+		}
+		
+		if(keyw.length > 0){
+			sKeyw = keyw[0];
+			for(int j = 1; j < keyw.length; j++){
+				sKeyw = sKeyw + " - " + keyw[j];
+			}
+		}
+
+		if(!Database.matchDB(comp[0], "COMPANY", "NAME")){
+			String cInject = "INSERT INTO COMPANY(NAME, SIMANUMER, EMAIL) VALUES('"
+							 + comp[0] + "', '" + comp[1] + "', '" + comp[2] + "');";
+			Database.insert(cInject);
+		}
+		
+		String inject = "INSERT INTO DAYTRIPS(NAME,COMPANY,PRICE,DESCR,KEYWORDS,CATEGORY,HOTELPICKUP) "
+						+ "VALUES('" + name + "', '" + comp[0] + "', '" + price + "', '" + desc + "', '"
+						+ sKeyw + "', '" + cat + "', '" + sPickup + "');";
+		
+    	Database.insert(inject);
     }
 
     @Override
