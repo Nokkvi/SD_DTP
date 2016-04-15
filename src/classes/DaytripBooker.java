071391d050628ee1b2f4cec29c0295947b2f5fc3
MrZoinker <mrzoinker@gmail.com>
2016-04-15 16:51:09 +0000

    Varð út undan

--- src/classes/DaytripBooker.java
+++ src/classes/DaytripBooker.java
@@ -1,17 +1,28 @@
 package classes;
-import java.util.*;
 
 public class DaytripBooker {
 
-	public static BookingOutput bookTrip(User user, List<String> dealerInfo, int price, List<String> review, DaytripExtend tripInfo, int seats){
+	public static void bookTrip(User user, DaytripExtend tripInfo, int seats){
 		BookInfoList b = new BookInfoList();
 		DaytripSearcher s = new DaytripSearcher();
 		int ID = tripInfo.getIndivId();
 		IndivDayTrip trip = s.searchByID(ID);
 		BookingInfo booking = new BookingInfo(user, trip, seats);
 		b.addBooking(booking);
+	}
+	
+	public static BookingOutput getBookingInfo(User usr){
+		BookInfoList b = new BookInfoList();
+		BookingInfo[] book = b.pullBooking();
+		BookingInfo theBook;
+		for(int i = 0; i < book.length; i++){
+			String name = book[i].getUser().getName();
+			
+			if(name.equals(usr)){
+				theBook = book[i];
+			}
+		}
 		
-		return null;
 	}
 	
 	public static void main(String[] args) {
